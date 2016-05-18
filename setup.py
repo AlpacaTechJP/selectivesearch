1d9d661dd03c6f66f62b3f32865eadd4b5afe304
Yuki Hayashi <yuki@alpacadb.com>
2016-05-18 16:35:24 +0900

    Bump the version to 0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="selectivesearch",
-    version="0.1",
+    version="0.2",
     url="https://github.com/AlpacaDB/selectivesearch",
     description="Selective Search implementation for Python",
     author="AlpacaDB, Inc.",
